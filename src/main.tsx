656b9811c8d651cf74cb7f547db0502f27b7e5fb
pablo-snz <pablo.snz.brm@gmail.com>
2023-12-12 13:37:41 +0100

    feat: adds mantine UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "@mantine/core/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import { MantineProvider } from "@mantine/core";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <MantineProvider defaultColorScheme="light">
     <App />
-  </React.StrictMode>,
-)
+  </MantineProvider>,
+);
